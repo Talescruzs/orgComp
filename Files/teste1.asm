07c8d4cbd13856412bb361af1b97f090c9438ca9
Talescruzs <tcs2017@hotmail.com>
2024-06-11 17:27:18 -0300

    add teste direito tambem

--- Files/teste1.asm
+++ Files/teste1.asm
@@ -1,23 +1,39 @@
 .text        
-
 .globl main
 main:	
+#################################
+#      TESTE DE ADD E ADDI	#
+#################################
 	addi	$sp, $sp, -8
 	addi	$t0, $0, 15
 	addi	$t1, $0, 5
 	add	$t2, $t0, $t1
+#################################
+#      TESTE DE SW E PILHA	#
+#################################
 	sw	$t2, 0($sp)
 	
+#################################
+#    TESTE DE ADDU E ADDIU	#
+#################################
 	addiu	$t0, $0, 15
 	addiu	$t1, $0, 5
 	addu	$t2, $t0, $t1
 	sw	$t2, 4($sp)
-	
+#################################
+#         TESTE DE LW    	#
+#################################
 	lw	$t0, 0($sp)
 	lw	$t1, 4($sp)
 	
 	addi	$sp, $sp, 8
+#################################
+#       TESTE DE DECISÃO    	#
+#################################
 	bne  	$t0, $t1, fracasso
+#################################
+#         TESTE DE J	    	#
+#################################
 	j	teste_loop
 loop_mentira:
 	j	fracasso
@@ -26,13 +42,25 @@ teste_loop:
 	loop:
 	add	$t1, $t1, $t1
 	bne 	$t1, $t0, sai_loop
+#################################
+#         TESTE DE JR	    	#
+#################################
 	jr	$ra
 	j	fracasso
 	fora:
+#################################
+#        TESTE DE MUL	    	#
+#################################
 	mul	$t2, $t1, $t0
+#################################
+#        TESTE DE JAL	    	#
+#################################
 	jal	loop
 	j	fracasso
 	sai_loop:
+#################################
+#   TESTE DE LI, LA E SYSCALL	#
+#################################
 sucesso:
 	li	$v0, 4
 	la	$a0, sucesso_teste1
@@ -47,5 +75,5 @@ fracasso:
 	syscall 
 
 .data
-sucesso_teste1:	.asciiz "\nTodos os testes funcionaram ;)\n"
-fracasso_teste1:.asciiz "\nNão passou no teste\n"
+sucesso_teste1:	.asciiz "Todos os testes funcionaram "
+fracasso_teste1:.asciiz "Não passou no teste"
